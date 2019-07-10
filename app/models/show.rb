f7a05501e6e9c096c1f6be0a663ab761893d00d9
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-10 01:21:49 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.highest_rating.each {|show| show }
+    Show.find_by_sql("SELECT maximum(:rating) FROM shows")
   end
 
   def self.lowest_rating
